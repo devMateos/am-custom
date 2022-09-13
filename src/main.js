7f7846abb741c07ee3f0dc5a6451591f3524f887
Antonio Mateos <antoniomateosmariscal@gmail.com>
2022-09-13 19:58:31 +0200

    Creating interactive mobile menu

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const menuButton = document.querySelector(".menu-button");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuButton.addEventListener("click", () => {
     menuButton.classList.toggle("nav-open");
+    mobileMenu.classList.toggle("slide-rigth");
 });
